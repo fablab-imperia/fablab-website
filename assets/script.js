bced6c1482aba8297f352c881146e556251490d5
Enrico Gallesio <lanzato@gmail.com>
2021-02-08 00:20:31 +0100

    few improvements, tagcloud added. Still a WIP tho

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 
 var clickHandler = function () {
   console.log('Click just happened');
+  // document.documentElement.style.setProperty('--primary-color', '225533');
+  // document.documentElement.style.setProperty('--second-color', 'CF250E');
+  // document.documentElement.style.setProperty('--third-color', '025b9f');
+  // document.documentElement.style.setProperty('--background-semitransp-color', 'rgba(10,69,10,0.42');
+
+  // document.documentElement.style.setProperty('--primary-linear-gradient', 'linear-gradient(180.55deg, #f18730 0.48%, #754207 99.52%)');
+  // document.documentElement.style.setProperty('--background-linear-gradient', 'linear-gradient(180deg, #c9b171 -2.22%, #86571a 97.78%)');
 };
 
 var onDocumentLoaded = function() {
@@ -10,3 +17,4 @@ var onDocumentLoaded = function() {
 
 window.addEventListener('load', onDocumentLoaded);
 
+ 
