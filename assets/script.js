3ee7a9adea3c2f19baead36e55c910f2c5d4fb41
Enrico Gallesio <lanzato@gmail.com>
2021-02-17 15:28:47 +0100

    navbar shrinking effect fixed

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,29 @@
 
-var clickHandler = function () {
+/* var clickHandler = function () {
   console.log('Click just happened');
-  // document.documentElement.style.setProperty('--primary-color', '225533');
-  // document.documentElement.style.setProperty('--second-color', 'CF250E');
-  // document.documentElement.style.setProperty('--third-color', '025b9f');
-  // document.documentElement.style.setProperty('--background-semitransp-color', 'rgba(10,69,10,0.42');
+  document.documentElement.style.setProperty('--primary-color', '225533');
+  document.documentElement.style.setProperty('--second-color', 'CF250E');
+  document.documentElement.style.setProperty('--third-color', '025b9f');
+  document.documentElement.style.setProperty('--background-semitransp-color', 'rgba(10,69,10,0.42');
 
-  // document.documentElement.style.setProperty('--primary-linear-gradient', 'linear-gradient(180.55deg, #f18730 0.48%, #754207 99.52%)');
-  // document.documentElement.style.setProperty('--background-linear-gradient', 'linear-gradient(180deg, #c9b171 -2.22%, #86571a 97.78%)');
-};
+  document.documentElement.style.setProperty('--primary-linear-gradient', 'linear-gradient(180.55deg, #f18730 0.48%, #754207 99.52%)');
+  document.documentElement.style.setProperty('--background-linear-gradient', 'linear-gradient(180deg, #c9b171 -2.22%, #86571a 97.78%)');
+}; */
 
 var onDocumentLoaded = function() {
-  var element = document.getElementById('themesel');
-  element.addEventListener('click', clickHandler);
+  // var element = document.getElementById('themesel');
+  // element.addEventListener('click', clickHandler);
+  window.onscroll = function() {scrollFunction()};
 };
+
+
+function scrollFunction() {
+  window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 200;  // > 0 has too much delay when scrolling back to top
+    header.classList.toggle('scrolling-active', windowPosition);
+  })
+}
 
 window.addEventListener('load', onDocumentLoaded);
 
