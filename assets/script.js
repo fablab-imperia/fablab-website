af58f38229efd485a0fa13d671ab7bcf17a5973f
Enrico Gallesio <lanzato@gmail.com>
2021-04-22 10:31:24 +0200

    wipeout for v.3

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
-var onDocumentLoaded = function() {
-    // var element = document.getElementById('themesel');
-    // element.addEventListener('click', clickHandler);
-    window.onscroll = function() {scrollFunction()};
+// var onDocumentLoaded = function() {
+//     // var element = document.getElementById('themesel');
+//     // element.addEventListener('click', clickHandler);
+//     window.onscroll s= function() {scrollFunction()};
 
-    // const lab_bg = document.querySelector('.laboratory');
-    // lab_bg.addEventListener('mouseover', function() {
-    //   console.log("CIAO");
-    //   lab_bg.classList.toggle('laboratory-hover');
-    //   // lab_bg.style.backgroundImage = "url(imgs/IMG_2880cropped2.jpg)";
-    // })
+//     // const lab_bg = document.querySelector('.laboratory');
+//     // lab_bg.addEventListener('mouseover', function() {
+//     //   console.log("CIAO");
+//     //   lab_bg.classList.toggle('laboratory-hover');
+//     //   // lab_bg.style.backgroundImage = "url(imgs/IMG_2880cropped2.jpg)";
+//     // })
 
-  };
+//   };
   
   
-  function scrollFunction() {
-    window.addEventListener('scroll', function () {
-      let header = document.querySelector('header');
-      let windowPosition = window.scrollY > 10;  // > 0 has too much delay when scrolling back to top
-      header.classList.toggle('navbar-scrolling', windowPosition);
-    })
-  }
+//   function scrollFunction() {
+//     window.addEventListener('scroll', function () {
+//       let header = document.querySelector('header');
+//       let windowPosition = window.scrollY > 10;  // > 0 has too much delay when scrolling back to top
+//       header.classList.toggle('navbar-scrolling', windowPosition);
+//     })
+//   }
   
-  window.addEventListener('load', onDocumentLoaded);
+//   window.addEventListener('load', onDocumentLoaded);
   
