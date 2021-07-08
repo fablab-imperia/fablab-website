8b2870f74b184fa9b339e7bc0c9e528ba7617af0
Enrico Gallesio <lanzato@gmail.com>
2021-07-08 10:29:47 +0200

    scroll to top button added

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
+    // We select the element we want to target
+    var target = document.getElementById("contacts");
+
+    var scrollToTopBtn = document.getElementById("totopbtn")
+    var rootElement = document.documentElement
+
+    // Next we want to create a function that will be called when that element is intersected
+    function callback(entries, observer) {
+    // The callback will return an array of entries, even if you are only observing a single item
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        // Show button
+        scrollToTopBtn.classList.add("show")
+        // console.log("show")
+        
+        } else {
+        // Hide button
+        scrollToTopBtn.classList.remove("show")
+        // console.log("unshow")
+        }
+    });
+    }
+
+    function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+    }
+    scrollToTopBtn.addEventListener("click", scrollToTop);
+        
+    // Next we instantiate the observer with the function we created above. This takes an optional configuration
+    // object that we will use in the other examples.
+    let observer = new IntersectionObserver(callback);
+    // Finally start observing the target element
+    observer.observe(target);
+
 const cookieStorage = {
     getItem: (item) => {
         const cookies = document.cookie
